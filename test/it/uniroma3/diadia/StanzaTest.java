3e22f5ec0dbff08bb39ef0b597f860d947a06cb5
Fill94 <fillo.musmeci@gmail.com>
2023-03-19 23:27:45 +0100

    added other uni test in StanzaTest

--- test/it/uniroma3/diadia/StanzaTest.java
+++ test/it/uniroma3/diadia/StanzaTest.java
@@ -1,5 +1,5 @@
 package it.uniroma3.diadia;
-
+import it.uniroma3.diadia.*;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -9,14 +9,18 @@ public class StanzaTest {
 	private Stanza stanzatest;
 	private Stanza stanzaAdiacente;
 	private Stanza stanzaAdiacente2;
+	private Attrezzo attrezzoTest;
 	private static final String NORD = "nord";
 	private static final String SUD = "sud";
+	
 	@Before
 	public void setUp() {
 		this.stanzatest = new Stanza("stanzaTest");
 		this.stanzaAdiacente = new Stanza("stanzaAdiacenteTest");
 		this.stanzaAdiacente2 = new Stanza("stanzaAdiacenteTest2");
+		this.attrezzoTest = new Attrezzo("attrezzoTest", 1);
 	}
+	
 	@Test
 	public void testImpostaStanzaAdiacenteNessunaStanzaAdiacente() {
 		assertEquals(0, this.stanzatest.getNumeroStanzeAdiacenti());
@@ -33,6 +37,7 @@ public class StanzaTest {
 		assertEquals("stanzaAdiacenteTest", this.stanzatest.getStanzaAdiacente(NORD).getNome());
 		assertEquals("stanzaAdiacenteTest2", this.stanzatest.getStanzaAdiacente(SUD).getNome());
 	}
+	
 	@Test
 	public void testImpostaStanzaAdiacenteSovrascriviStanzaAdiacente() {
 		this.stanzatest.impostaStanzaAdiacente(NORD, stanzaAdiacente);
@@ -43,25 +48,60 @@ public class StanzaTest {
 	}
 	
 	@Test
-	public void testGetStanzaAdiacente() {
-		fail("Not yet implemented");
+	public void testGetStanzaAdiacenteNessunaStanzaAdiacente() {
+		assertNull(this.stanzatest.getStanzaAdiacente(NORD));
+	}
+	
+	@Test
+	public void testGetStanzaAdiacenteEsisteStanzaAdiacente() {
+		this.stanzatest.impostaStanzaAdiacente(NORD, stanzaAdiacente);
+		assertSame(stanzaAdiacente, this.stanzatest.getStanzaAdiacente(NORD)); 
 	}
 
 	@Test
-	public void testAddAttrezzo() {
-		fail("Not yet implemented");
+	public void testAddAttrezzoNessunAttrezzo() {
+		assertTrue(this.stanzatest.addAttrezzo(attrezzoTest));
+		assertEquals(1, this.stanzatest.getNumeroAttrezzi());
+	}
+	
+	@Test
+	public void testAddAttrezzoUnAttrezzo() {
+		assertTrue(this.stanzatest.addAttrezzo(attrezzoTest));
+		assertEquals(1, this.stanzatest.getNumeroAttrezzi());
+		assertTrue(this.stanzatest.addAttrezzo(attrezzoTest));
+		assertEquals(2, this.stanzatest.getNumeroAttrezzi());
+	}
+	
+	
+	@Test
+	public void testAddAttrezzoNumeroMassimoAttrezzi() {
+		for(int i = 0 ; i < 10 ; i++ )
+			this.stanzatest.addAttrezzo(new Attrezzo("attrezzoTest"+i , 1));
+		assertEquals(10, this.stanzatest.getNumeroAttrezzi());
+		assertFalse(this.stanzatest.addAttrezzo(attrezzoTest));
 	}
 
 	@Test
-	public void testHasAttrezzo() {
-		fail("Not yet implemented");
+	public void testHasAttrezzoNonEsistente() {
+		assertFalse(this.stanzatest.hasAttrezzo("attrezzoTest"));
+	}
+	
+	@Test
+	public void testHasAttrezzoEsistente() {
+		this.stanzatest.addAttrezzo(attrezzoTest);
+		assertTrue(this.stanzatest.hasAttrezzo("attrezzoTest"));
 	}
 
 	@Test
-	public void testGetAttrezzo() {
-		fail("Not yet implemented");
+	public void testGetAttrezzoInesistente() {
+		assertNull(this.stanzatest.getAttrezzo("attrezzoTest"));
 	}
-
+	
+	@Test
+	public void testGetAttrezzoEsistente() {
+		this.stanzatest.addAttrezzo(attrezzoTest);
+		assertSame(this.attrezzoTest, this.stanzatest.getAttrezzo("attrezzoTest"));
+	}
 	@Test
 	public void testRemoveAttrezzo() {
 		fail("Not yet implemented");
